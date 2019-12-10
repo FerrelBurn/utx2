82b6079099d130e720fe7037176ab0873c3d19e0
Joel Harris <joel@zapatatechnology.com>
2019-12-10 16:28:53 -0500

    startup script

--- startup.sh
+++ startup.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 echo "Welcome"
 echo "checking to see if this script has ran before"
-FILE=/etc/.startup
+FILE=/opt/zapataai
 if test -f "$FILE"; then
   echo "$FILE exists which means it has ran before... exiting..."
   exit
   else
-  apt get install python3-pip
-  apt get install docker-compose
-  sed -i '/ExecStart=/usr/bin/dockerd/c\ExecStart=/usr/bin/dockerd -H fd:// -H=tcp://0.0.0.0:5555 --containerd=/run/containerd/containerd.sock' /lib/systemd/system/docker.service
-  systemctl daemon-reload
-  service docker restart
-  chown -R nvidia:nvidia /home/nvidia/.cache/
-  usermod -aG docker $USER
+    mkdir $FILE
+    chown -R nvidia:nvidia /opt/zapataai
+    apt get install python3-pip
+    apt get install docker-compose
+    sed -i '/ExecStart=/usr/bin/dockerd/c\ExecStart=/usr/bin/dockerd -H fd:// -H=tcp://0.0.0.0:5555 --containerd=/run/containerd/containerd.sock' /lib/systemd/system/docker.service
+    systemctl daemon-reload
+    service docker restart
+    chown -R nvidia:nvidia /home/nvidia/.cache/
+    usermod -aG docker $USER
+    git clone https://github.com/FerrelBurn/utx2.git /opt/zapataai/
+    chown -R nvidia:nvidia /opt/zapataai
+    chmod 774 /opt/zapataai/setup-containers.sh
+    sh /opt/zapataai/setup-containers.sh
 
-  touch $FILE
+
 fi
