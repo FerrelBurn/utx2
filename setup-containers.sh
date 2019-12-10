e5ac313ab682acaa10e30debef6f8a9c9aa0a203
Joel Harris <joel@zapatatechnology.com>
2019-12-10 17:31:21 -0500

    sup

--- setup-containers.sh
+++ setup-containers.sh
@@ -5,5 +5,5 @@ echo "this script will pull the docker images needed and start them"
 echo "don't re-download if you don't need to"
 
 docker-compose -f /opt/zapataai/docker-compose.yaml up -d
-pip install -r /opt/zapataai/requirements.txt
+pip3 install -r /opt/zapataai/requirements.txt
 python /opt/zapataai/heartbeat.py &amp;
