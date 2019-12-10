af1627937c9f470cd32aae5de278ccafe32f2516
Joel Harris <joel@zapatatechnology.com>
2019-12-10 17:07:32 -0500

    sup

--- setup-containers.sh
+++ setup-containers.sh
@@ -4,4 +4,4 @@ echo "getting ready to get some containers"
 echo "this script will pull the docker images needed and start them"
 echo "don't re-download if you don't need to"
 
-docker-compose /opt/zapataai/docker-compose.yaml up -d
+docker-compose -f /opt/zapataai/docker-compose.yaml up -d
