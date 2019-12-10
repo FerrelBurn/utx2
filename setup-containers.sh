431ebd1b451b6f550db4fbb848dce9982a040f77
Joel Harris <joel@zapatatechnology.com>
2019-12-10 17:35:15 -0500

    sup

--- setup-containers.sh
+++ setup-containers.sh
@@ -6,4 +6,4 @@ echo "don't re-download if you don't need to"
 
 docker-compose -f /opt/zapataai/docker-compose.yaml up -d
 pip3 install -r /opt/zapataai/requirements.txt
-python /opt/zapataai/heartbeat.py &amp;
+python /opt/zapataai/heartbeat.py &
