2ee45476c13aa1b8a6b0c7ea408b7b039345a73c
Joel Harris <joel@zapatatechnology.com>
2019-12-10 17:14:41 -0500

    sup

--- setup-containers.sh
+++ setup-containers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo "******************************************************************"
 echo "getting ready to get some containers"
 echo "this script will pull the docker images needed and start them"
 echo "don't re-download if you don't need to"
