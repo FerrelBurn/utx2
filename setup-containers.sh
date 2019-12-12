b55d5766ee381a6d8e8d25e3b865c2b42d2a1195
Joel Harris <joel@zapatatechnology.com>
2019-12-12 16:02:18 -0500

    changed rest server IP back to my computer

--- setup-containers.sh
+++ setup-containers.sh
@@ -2,7 +2,7 @@
 echo "******************************************************************"
 echo "getting ready to get some containers"
 echo "this script will pull the docker images needed and start them"
-echo "don't re-download if you don't need to  blah2"
+echo "don't re-download if you don't need to  blah3"
 
 docker-compose -f /opt/zapataai/docker-compose.yaml up -d
 pip3 install -r /opt/zapataai/requirements.txt
