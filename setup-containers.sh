aa8de7cbf34b0f68253d5f325c9a0714ca159561
Joel Harris <joel@zapatatechnology.com>
2019-12-13 00:20:03 -0500

    fixing git issues

--- setup-containers.sh
+++ setup-containers.sh
@@ -5,7 +5,7 @@ echo "this script will pull the docker images needed and start them"
 echo "don't re-download if you don't need to  blah3"
 
 docker-compose -f /opt/zapataai/docker-compose.yaml up -d
-echo python3 --version
+python3 --version
 which python3
 install -r /opt/zapataai/requirements.txt
 python3 /opt/zapataai/heartbeat.py &
