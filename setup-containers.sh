beb18dffebbbd80d276ebede2021451685475446
Joel <joel@zapatatechnology.com>
2020-03-12 18:03:24 -0400

    added line to change notebook folder permissions

--- setup-containers.sh
+++ setup-containers.sh
@@ -4,7 +4,7 @@ echo "******************************************************************"
 echo "getting ready to get some containers"
 echo "this script will pull the docker images needed and start them"
 echo "don't re-download if you don't need to  blah3"
-
+chmod 775 notebooks
 docker-compose -f /opt/zapataai/docker-compose.yaml up -d
 /usr/bin/python3 --version
 which python3
