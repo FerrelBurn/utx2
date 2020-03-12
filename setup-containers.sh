f8dd7d97f26ed7d935b5484caf065c474dabe9ae
Joel <joel@zapatatechnology.com>
2020-03-12 18:03:24 -0400

    Added reply to /submit endpoint showing JSON paylod of POST

--- setup-containers.sh
+++ setup-containers.sh
@@ -4,12 +4,12 @@ echo "******************************************************************"
 echo "getting ready to get some containers"
 echo "this script will pull the docker images needed and start them"
 echo "don't re-download if you don't need to  blah3"
-
+chmod 775 notebooks
 docker-compose -f /opt/zapataai/docker-compose.yaml up -d
 /usr/bin/python3 --version
 which python3
 which pip3
-/usr/local/bin/pip3 install -r /opt/zapataai/requirements.txt
+/usr/local/bin/pip install -r /opt/zapataai/requirements.txt
 chmod +x /opt/zapataai/heartbeat.py
 /usr/bin/python3 /opt/zapataai/heartbeat.py 
 echo "setup-containers script completed"
